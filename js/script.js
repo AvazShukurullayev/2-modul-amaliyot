350728920a0f0aa1c422df11f5bad93c7b14f471
Avaz <elonarm21@gmail.com>
2024-03-03 02:26:35 +0500

    Amaliyot. Fetch Telegram BOT

--- js/script.js
+++ js/script.js
@@ -102,8 +102,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // Modal
     const modalOpenBtns = document.querySelectorAll("[data-modal]"),
-        modal = document.querySelector(".modal"),
-        modalCloseBtn = document.querySelector("[data-modal-close]")
+        modal = document.querySelector(".modal")
+
+    // modalCloseBtn = document.querySelector("[data-modal-close]")
 
     function openModal() {
         modal.classList.add("show", "fade")
@@ -123,10 +124,10 @@ window.addEventListener("DOMContentLoaded", () => {
         btn.addEventListener("click", openModal)
     })
 
-    modalCloseBtn.addEventListener("click", closeModal)
+    // modalCloseBtn.addEventListener("click", closeModal)
 
     modal.addEventListener("click", (event) => {
-        if (event.target === modal) {
+        if (event.target === modal || event.target.getAttribute("data-modal-close") === "") {
             closeModal()
         }
     })
@@ -137,7 +138,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     })
 
-    const modalTimerId = setTimeout(openModal, 6000)
+    const modalTimerId = setTimeout(openModal, 600000)
 
     // Class Offer Menu
     class OfferMenu {
@@ -261,6 +262,69 @@ window.addEventListener("DOMContentLoaded", () => {
         const {src, alt, title, time} = item
         new EatingTime(src, alt, title, time, ".daytime-items").render()
     })
+
+    // Form
+    const form = document.querySelector("form"),
+        telegramBotToken = "7019010563:AAFdy3Z56hkrcUT5xUb8I2KaE-h-cDDf_Ew",
+        chatId = "826125853"
+
+    const message = {
+        loading: "Loading...",
+        successful: "Thanks for contacting with us",
+        failure: "Something went wrong"
+    }
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const loader = document.createElement("span")
+        loader.classList.add("loader")
+        loader.style.width = "30px"
+        loader.style.height = "30px"
+        loader.style.marginTop = "15px"
+        form.append(loader)
+        const formData = new FormData(form)
+        const object = {}
+        formData.forEach((value, key) => object[key] = value)
+        console.log(object)
+
+        // fetch POST request
+        fetch(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: `Name: ${object.name}. Phone: +${object.phone}`
+            })
+        })
+            .then(() => {
+                form.reset()
+                showStatusMessage(message.successful)
+            })
+            .catch(() => showStatusMessage(message.failure))
+            .finally(() => loader.remove())
+    })
+
+    function showStatusMessage(message) {
+        const modalDialog = document.querySelector(".modal__dialog")
+
+        modalDialog.classList.add("hide")
+        openModal()
+        const statusModal = document.createElement("div")
+        statusModal.classList.add("modal__dialog")
+        statusModal.innerHTML = `
+            <div class="modal__content">
+                <div data-modal-close class="modal__close">&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `
+        document.querySelector(".modal").append(statusModal)
+
+        setTimeout(() => {
+            statusModal.remove()
+            modalDialog.classList.remove("hide")
+            closeModal()
+        }, 4500)
+    }
 
     // Todo: Call Functions
     hideTabContents()
