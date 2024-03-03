9e88d3ee49eca1aa80b74b05e2840dd10a78275f
Avaz <elonarm21@gmail.com>
2024-03-03 05:23:01 +0500

    added slider to project

--- js/script.js
+++ js/script.js
@@ -338,8 +338,55 @@ window.addEventListener("DOMContentLoaded", () => {
         }, 4500)
     }
 
+    // Slider
+    const slides = document.querySelectorAll(".offer__slide"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        currentSlide = document.querySelector("#current"),
+        totalSlides = document.querySelector("#total")
+
+    let slideIndex = 1
+
+    if (slides.length < 10) {
+        totalSlides.textContent = `0${slides.length}`
+    } else {
+        totalSlides.textContent = slides.length
+    }
+
+    function showSlides(index) {
+        if (index > slides.length) {
+            slideIndex = 1
+        }
+        if (index < 1) {
+            slideIndex = slides.length
+        }
+        slides.forEach(slide => slide.style.display = "none")
+        slides[slideIndex - 1].style.display = "block"
+
+        if (slides.length < 10) {
+            currentSlide.textContent = `0${slideIndex}`
+        } else {
+            currentSlide.textContent = slideIndex
+        }
+    }
+
+    function moveSlides(index) {
+        showSlides(slideIndex += index)
+        currentSlide.textContent = formatSlide(slideIndex += index)
+    }
+
+    next.addEventListener("click", () => {
+        moveSlides(1)
+    })
+
+    prev.addEventListener("click", () => {
+        moveSlides(-1)
+    })
+
+
     // Todo: Call Functions
     hideTabContents()
     showTabContent()
     setClock(".timer", deadline)
+    showSlides(slideIndex)
 })
